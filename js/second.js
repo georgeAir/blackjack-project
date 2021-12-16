7798c2a74b033ca2d42c46207be2c72b6bdec184
george-arias <georgearias08@gmail.com>
2021-12-16 15:05:47 -0600

    added a commentar div

--- js/second.js
+++ js/second.js
@@ -6,11 +6,11 @@ let deck = [2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11,
             2,3,4,5,6,7,8,9,10,10,10,10,11];
 
-let wallet = 500
+let wallet = 100
 let playersHand
 let dealersHand
 
-const startButton = document.querySelector('#start')
+const dealButton = document.querySelector('#start')
 const hitButton = document.querySelector('#hit')
 const stayButton = document.querySelector('#stay')
 
@@ -22,6 +22,10 @@ const playerSide = document.querySelector('#player')
 // playerSide.innerText = playerHand
 const playersValue = document.querySelector('#playersValue')
 
+const walletAmount = document.querySelector('#walletAmount')
+
+const commentary = document.querySelector('#commentaryBox')
+// commentary.innerText = "21! Player wins on the draw!"
 
 
 /////////////////////// Draw Cards Function  ///////////////////////////////
@@ -33,12 +37,13 @@ function drawCards(deck){
 console.log(drawCards(deck));
 /////////////////////// Player And Dealers Funtion //////////////////////////////
 function dealHands(){
-playersHand =[drawCards(deck) , drawCards(deck)]
-dealersHand =[drawCards(deck), drawCards(deck)]
-console.log('Your cards are '+ playersHand);
-console.log(`Your hand value: ` + handValue(playersHand));
-console.log(`Dealer's cards are `+ dealersHand);
-console.log(`Dealer's hand value: ` + handValue(dealersHand));
+  wallet -= 5
+  playersHand =[drawCards(deck) , drawCards(deck)]
+  dealersHand =[drawCards(deck), drawCards(deck)]
+  console.log('Your cards are '+ playersHand);
+  console.log(`Your hand value: ` + handValue(playersHand));
+  console.log(`Dealer's cards are `+ dealersHand);
+  console.log(`Dealer's hand value: ` + handValue(dealersHand));
 // compareHands()
 }
 // start()
@@ -62,7 +67,8 @@ function startGame(){
   dealHands()
   if (handValue(playersHand)=== 21){
       console.log('21! Player wins on the draw!');
-      alert('21! Player wins on the draw!');
+      commentary.innerText = "21! Player wins on the draw!"
+      wallet += 10
   } else if (handValue(dealersHand)===21){
     console.log('21! Dealer wins on the draw!');
     alert(' 21! Dealer wins on the draw!');
@@ -84,46 +90,76 @@ playerSide.innerText = playersHand
 
 dealersValue.innerText= handValue(dealersHand)
 playersValue.innerText= handValue(playersHand)
+walletAmount.innerText = wallet
+
 
 /////////////////////// HIT Function/Button  ///////////////////////////////
 
 function hit (){
   playersHand.push(drawCards(deck))
-  console.log(`Your new hand: ` + playersHand);
-  console.log(`Your hand value: ` + handValue(playersHand));
+  dealerSide.innerText = dealersHand
+  playerSide.innerText = playersHand
+
+  dealersValue.innerText= handValue(dealersHand)
+  playersValue.innerText= handValue(playersHand)
+
+  walletAmount.innerText = wallet
   if (handValue(playersHand) === 21){
     console.log('21! player wins!');
     alert('21! player wins!');
+    wallet += 10
   }else if(handValue(playersHand) > 21){
     console.log('Player BUSTS!');
     alert('Player BUSTS!');
 
-    dealerSide.innerText = dealersHand
-    playerSide.innerText = playersHand
+    // dealerSide.innerText = dealersHand
+    // playerSide.innerText = playersHand
+    //
+    // dealersValue.innerText= handValue(dealersHand)
+    // playersValue.innerText= handValue(playersHand)
+    // walletAmount.innerText = wallet
 
-    dealersValue.innerText= handValue(dealersHand)
-    playersValue.innerText= handValue(playersHand)
-    // let endGame = alert('YOU BUSTED!')
   }
 }
+
+
+
 function dealerHit (){
   dealersHand.push(drawCards(deck))
+  dealerSide.innerText = dealersHand
+  playerSide.innerText = playersHand
+
+  dealersValue.innerText= handValue(dealersHand)
+  playersValue.innerText= handValue(playersHand)
+
+  walletAmount.innerText = wallet
   console.log(`Dealer's new hand: ` + dealersHand);
   console.log(`Dealer's hand value: ` + handValue(dealersHand));
   if (handValue(dealersHand) === 21){
     console.log('21! dealer wins!');
     alert('21! Dealer wins!')
+
   }else if(handValue(dealersHand) > 21){
     console.log('DEALER BUSTS!');
     alert('DEALER BUSTS!')
+    wallet += 10
 
-    dealerSide.innerText = dealersHand
-    playerSide.innerText = playersHand
+    // dealerSide.innerText = dealersHand
+    // playerSide.innerText = playersHand
+    //
+    // dealersValue.innerText= handValue(dealersHand)
+    // playersValue.innerText= handValue(playersHand)
+    //
+    // walletAmount.innerText = wallet
 
-    dealersValue.innerText= handValue(dealersHand)
-    playersValue.innerText= handValue(playersHand)
-    // let endGame = alert('YOU BUSTED!')
   }
+  dealerSide.innerText = dealersHand
+  playerSide.innerText = playersHand
+
+  dealersValue.innerText= handValue(dealersHand)
+  playersValue.innerText= handValue(playersHand)
+
+  walletAmount.innerText = wallet
 }
 hitButton.addEventListener('click', (event) => {
 hit()
@@ -147,21 +183,22 @@ function compareHands(){
   if (handValue(playersHand) <= 21 && handValue(playersHand) > handValue(dealersHand)){
     console.log('Player wins!');
     alert('Player wins!')
-    wallet += 5
+    wallet += 10
     console.log(wallet);
   }else if (handValue(dealersHand)<= 21 && handValue(dealersHand) > handValue(playersHand)) {
     console.log('Dealer wins!');
     alert('Dealer wins!')
+
   }
 }
 /////////////// Check for 21  ///////////////
-function checkFor21 (){
-  if (handValue(playersHand) === 21){
-    console.log('player wins');
-  }else if (handValue(dealersHand)=== 21){
-    console.log('dealer wins');
-  }
-}
+// function checkFor21 (){
+//   if (handValue(playersHand) === 21){
+//     console.log('player wins');
+//   }else if (handValue(dealersHand)=== 21){
+//     console.log('dealer wins');
+//   }
+// }
 //////////////// ACCESSING THE DOM ////////////////
 
 dealerSide.innerText = dealersHand
@@ -170,12 +207,17 @@ playerSide.innerText = playersHand
 dealersValue.innerText= handValue(dealersHand)
 playersValue.innerText= handValue(playersHand)
 
-startButton.addEventListener('click', (event) => {
-console.log('click');
+walletAmount.innerText = wallet
+
+
+dealButton.addEventListener('click', (event) => {
 dealHands()
 dealerSide.innerText = dealersHand
 playerSide.innerText = playersHand
 
 dealersValue.innerText= handValue(dealersHand)
 playersValue.innerText= handValue(playersHand)
+
+walletAmount.innerText = wallet
+
 })
